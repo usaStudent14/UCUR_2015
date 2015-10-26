458b57bb1ddd8c97b0a9b339082ba2921f78aede
usaStudent14 <ach1223@jagmail.southalabama.edu>
2015-10-26 15:16:42 -0500

    New version of pc code for game

---
+++ GamePCContest/GamePC/Robot.cpp
@@ -0,0 +1,40 @@
+#include "Robot.h"
+#include <iostream>
+
+void Robot::setPos(int x, int y){
+	currentPos.x = x;
+	currentPos.y = y;
+}
+
+void Robot::setTarg(int x, int y){
+	targ.x = x;
+	targ.y = y;
+}
+
+coords Robot::getPos(){
+	return currentPos;
+}
+
+coords Robot::getTarg(){
+	return targ;
+}
+
+bool Robot::compareTarg(coords rTarg){
+	if(rTarg.x == targ.x && rTarg.y == targ.y)
+		return true;
+	else
+		return false;
+}
+
+void Robot::incrementMoves(){
+	moves++;
+}
+
+void Robot::incrementScore(){
+	score++;
+}
+
+int Robot::getMoves(){
+	return moves;
+}
+
